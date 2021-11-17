25d710f3f8c133d8a219aeb0c033fd837a6753bb
Madhu <madhuponnam66@gmail.com>
2021-11-17 11:39:45 +0530

    working done .
    need to add styling

--- api.js
+++ api.js
@@ -1,8 +1,22 @@
 var btnTRanslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#txt-input");
 var output=document.querySelector("#output");
-function clickHandler(){
-    output.innerText="hdbasfggf "+textInput.value
-    
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
+function getTranslation(text){
+    return serverUrl+ "?" + "text=" + text
 }
+function errorHandler(error){
+    console.log("Error occured",error)
+    alert("something wrong with server!Try again after sometime")
+}
+function clickHandler(){
+   var inputtext=textInput.value;
+   fetch(getTranslation(inputtext))
+   .then(response => response.json())
+   .then(json => {
+      var translatedtext=json.contents.translated;
+      output.innerHTML=translatedtext;
+   })
+    .catch(errorHandler)
+    }
 btnTRanslate.addEventListener("click",clickHandler)
